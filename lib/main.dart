0459a7321659b9264c86da4f25fe68f8ffcafbc5
Graziela Souza <118382634+grazisou00@users.noreply.github.com>
2024-07-01 14:37:56 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
-      ),
+      ),  
       home: TaskListScreen(),
     );
   }
